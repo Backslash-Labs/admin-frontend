a3a6687b844acd448c3a4bab584d54c365ed6b85
Wendy Quarshie <42671174+codebuggg@users.noreply.github.com>
2023-04-22 23:51:01 +0200

    menu items

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -24,11 +24,11 @@ const user = {
     'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
 }
 const navigation = [
-  { name: 'Dashboard', href: '#', current: true },
-  { name: 'Team', href: '#', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false },
-  { name: 'Reports', href: '#', current: false },
+  { name: 'Restaurants', href: '#', current: true },
+  { name: 'Branches', href: '#', current: false },
+  { name: 'Users', href: '#', current: false },
+  { name: 'Billing', href: '#', current: false },
+  // { name: 'Reports', href: '#', current: false },
 ]
 const userNavigation = [
   { name: 'Your Profile', href: '#' },
