469b716769de27cd6b72b559bdbb27c040e420c7
Joshua <j.oduro@backslashlab.com>
2023-04-30 21:15:15 +0000

    feat: add header

--- src/hocs/withNav.tsx
+++ src/hocs/withNav.tsx
@@ -13,11 +13,11 @@ const withNav = (Component) => {
                 <div className="min-h-full">
                     <div className="bg-green-850 pb-32">
                         <Nav />
-                        {/* <header className="py-10">
-                                <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                        <header className="py-10">
+                            <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                                 <h1 className="text-3xl font-bold tracking-tight text-white">Dashboard</h1>
-                                </div>
-                        </header> */}
+                            </div>
+                        </header>
                     </div>
 
                     <main className="-mt-32">
