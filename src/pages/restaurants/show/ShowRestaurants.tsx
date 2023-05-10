6c0cffb0d983f5f9dc92ddcbd34aaf801816c02e
Joshua <j.oduro@backslashlab.com>
2023-05-10 18:26:01 +0000

    feat: fetch restaurant in show

--- src/pages/restaurants/show/ShowRestaurants.tsx
+++ src/pages/restaurants/show/ShowRestaurants.tsx
@@ -1,5 +1,7 @@
 import LoadingTable from "components/LoadingTable";
 import withNav from "hocs/withNav";
+import useFetch from "lib/useFetch";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const ShowRestaurants = () => {
@@ -10,8 +12,34 @@ const ShowRestaurants = () => {
 
     const path = `/restaurants/${id}/branches`;
 
+    const [restaurant, setRestaurant] = useState({});
+
+    const {
+        onFetch,
+        isFetching,
+    } = useFetch(`/admin/companies/${id}`, setRestaurant)
+
+    useEffect(() => {
+        onFetch()
+    }, [])
+
+
+    if (isFetching) return null;
+
     return (
         <>
+            <div className="px-4 sm:px-6 lg:px-8 mb-10">
+                <div className="sm:flex sm:items-center">
+                    <div className="sm:flex-auto">
+
+                        <h1>
+                            {restaurant.name}
+                        </h1>
+                        <p>{restaurant.email}</p>
+
+                    </div>
+                </div>
+            </div>
             <LoadingTable
                 path={`/admin/companies/${id}/branches`}
                 headers={[
