2e6ac93f9bbd8c2227afdfb8344bd36e28fee40a
Joshua <j.oduro@backslashlab.com>
2023-05-15 18:36:55 +0100

    build: update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,11 @@ export default {
     extend: {
       colors: {
         green: {
+          250: '#D3F9D8',
           850: '#008644',
+        },
+        gray: {
+          450: 'background: #343A40',
         }
       }
     },
--- tests/setupTest.ts
+++
@@ -1,6 +0,0 @@
-import fetchMock from "jest-fetch-mock";
-
-fetchMock.enableMocks();
-
-// @ts-ignore
-global.fetchMock = fetchMock;
