c7697f79878e4c9993492e97c9f388c7719bb316
Joshua <j.oduro@backslashlab.com>
2023-05-17 11:42:48 +0100

    tests: add invalid tests for RestaurantForm

--- tests/RestaurantForm.test.tsx
+++ tests/RestaurantForm.test.tsx
@@ -18,9 +18,56 @@ const routes = [
     }
 ];
 
+const cases = [
+    {
+        fields: {
+            key: "Name",
+            value: "",
+        },
+        expected: "name is a required field"
+    },
+    {
+        fields: {
+            key: "Email",
+            value: "",
+        },
+        expected: "email is a required field"
+    }
+]
+
+const data = [
+    {
+        key: "Name",
+        value: "Restaurant"
+    },
+    {
+        key: "Email",
+        value: "user@mail.com"
+    },
+    {
+        key: "Branch Location",
+        value: "Location",
+        name: "branch_location"
+    },
+    {
+        key: "Plan",
+        value: "1",
+        name: "plan_id"
+    },
+]
+
+let onAddRestaurant = async () => {
+    const button = screen.getByText("Add Restaurant");
+    return act(async () => {
+        await fireEvent.click(button)
+    });
+}
+
 describe("<RestaurantForm />", () => {
 
-    it("should render restaurants", async () => {
+    let router;
+
+    beforeEach(async () => {
         // @ts-ignore
         global.fetchMock.mockResponse(
             JSON.stringify([
@@ -40,19 +87,31 @@ describe("<RestaurantForm />", () => {
                 }
             ]),
         );
-        let router
         await act(async () => {
             router = await withRender(routes, routerOptions);
         });
-        onChange("Name", "Restaurant");
-        onChange("Email", "user@mail.com");
-        onChange("Branch Location", "Location", "branch_location");
-        onChange("Plan", "1", "plan_id");
-        const button = screen.getByText("Add Restaurant");
-        await act(async () => {
-            await fireEvent.click(button)
+    })
+
+    it("should redirect to home", async () => {
+
+        data.forEach(({ key, value, name }) => {
+            onChange(key, value, name);
         });
+        await onAddRestaurant();
         expect(router.state.location.pathname).toBe("/");
     });
+
+
+    describe("Invalid", () => {
+        test.each(cases)(`it should display $expected`, async ({ fields, expected }) => {
+            data.forEach(({ key, value, name }) => {
+                if (key === fields.key) onChange(key, fields.value, name);
+                else onChange(key, value, name);
+            });
+            await onAddRestaurant();
+            const t = screen.getByText(expected);
+            expect(t).not.toBeNull();
+        })
+    })
 
 })
